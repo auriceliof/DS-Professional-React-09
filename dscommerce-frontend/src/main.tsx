99cbd30215f216ea6981316796bdb2e3be22232a
auriceliof <auricelio.suporte@hotmail.com>
2024-04-11 16:00:15 -0300

    09-06 Aviso referência explícita do React

--- dscommerce-frontend/src/main.tsx
+++ dscommerce-frontend/src/main.tsx
@@ -1,6 +1,9 @@
+import './index.css'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import React from 'react'
+
+window.React = React
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
